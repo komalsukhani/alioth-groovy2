e29575fcffc02bd43a5a53af40aca20ed9fed6c2
Miguel Landaeta <miguel@miguel.cc>
2013-11-02 09:57:32 -0300

    Debian release 2.1.6+dfsg-1

---
+++ debian/orig-tar.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+# called by uscan with '--upstream-version' <version> <file>
+TAR=../groovy_$2+dfsg.orig.tar.gz
+DIR=groovy-$2
+
+# clean up the upstream tarball
+unzip $3
+rm -rf $DIR/build
+GZIP=--best tar -c -z -f $TAR --exclude '*.jar' --exclude '*.pdf' --exclude '*.class' $DIR --numeric-owner --owner 0 --group 0
+rm -rf $3 $DIR
+
+# move to directory 'tarballs'
+if [ -r .svn/deb-layout ]; then
+  . .svn/deb-layout
+  mv $TAR $origDir
+  echo "moved $TAR to $origDir"
+fi
