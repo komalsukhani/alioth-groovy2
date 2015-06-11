1da2edc12a566b8cac4de3c0f6cd3d0e30259d83
Emmanuel Bourg <ebourg@apache.org>
2015-06-11 09:18:33 +0200

    Use XZ compression for the upstream tarball

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 
 # called by uscan with '--upstream-version' <version> <file>
-TAR=../groovy2_$2+dfsg.orig.tar.gz
+TAR=../groovy2_$2+dfsg.orig.tar.xz
 DIR=groovy-$2
 
 # clean up the upstream tarball
 unzip $3
 rm -rf $DIR/build
-GZIP=--best tar -c -z -f $TAR --exclude '*.jar' --exclude '*.pdf' --exclude '*.class' $DIR --numeric-owner --owner 0 --group 0
+XZ_OPT=--best tar -c -J -f $TAR --exclude '*.jar' --exclude '*.pdf' --exclude '*.class' $DIR --numeric-owner --owner 0 --group 0
 rm -rf $3 $DIR
 
 # move to directory 'tarballs'
